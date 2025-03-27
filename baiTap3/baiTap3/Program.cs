0ba62b23f5a9a7d759819582162740f911865e93
Trần Minh Tuyên <trminhtuyen1905@gmail.com>
2025-03-27 21:28:55 +0700

    Add files via upload
    bài 3

---
+++ baiTap3/baiTap3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baiTap3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Nhập phần thực số phức A: ");
+            double aReal = double.Parse(Console.ReadLine());
+            Console.Write("Nhập phần số ảo số phức A: ");
+            double aImaginary = double.Parse(Console.ReadLine());
+
+            Console.Write("Nhập phần thực số phức B: ");
+            double bReal = double.Parse(Console.ReadLine());
+            Console.Write("Nhập phần số ảo số phức B: ");
+            double bImaginary = double.Parse(Console.ReadLine());
+
+            complexNumber A = new complexNumber(aReal, aImaginary);
+            complexNumber B = new complexNumber(bReal, bImaginary);
+
+            Console.WriteLine($"A + B = {A + B}");
+            Console.WriteLine($"A - B = {A - B}");
+            Console.WriteLine($"A * B = {A * B}");
+            Console.WriteLine($"A / B = {A / B}");
+        }
+    }
+}
---
+++ baiTap3/baiTap3/complexNumber.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baiTap3
+{
+    internal class complexNumber
+    {
+        public double Real { get; set; }
+        public double Imaginary { get; set; }
+
+        public complexNumber( double real, double imaginary)
+        {
+            Real = real;
+            Imaginary = imaginary;
+        }
+        public static complexNumber operator + (complexNumber a, complexNumber b)
+        {
+            return new complexNumber(a.Real + b.Real, a.Imaginary + b.Imaginary);
+        }
+        public static complexNumber operator - (complexNumber a, complexNumber b )
+        {
+            return new complexNumber(a.Real - b.Real, a.Imaginary - b.Imaginary);
+        }
+        public static complexNumber operator * (complexNumber a, complexNumber b)
+        {
+            return new complexNumber(a.Real * b.Real - a.Imaginary * b.Imaginary, a.Real * b.Imaginary + b.Real * a.Imaginary);
+        }
+        public static complexNumber operator /(complexNumber a, complexNumber b)
+        {
+            double denominator = b.Real * b.Real + b.Imaginary * b.Imaginary;
+            return new complexNumber((a.Real * b.Real + a.Imaginary * b.Imaginary) / denominator,
+                               (a.Imaginary * b.Real - a.Real * b.Imaginary) / denominator);
+        }
+        public override string ToString()
+        {
+            return $"{Real:F2} + {Imaginary:F2} ";
+        }
+
+    }
+}
